c055eda41e73303a9824bf6ab01005a55977a1de
mahmoud-goweda <mahmoudgoweda87@gmail.com>
2020-04-23 08:09:55 +0200

    RXJS part 2

--- src/app/service/dish.service.ts
+++ src/app/service/dish.service.ts
@@ -14,8 +14,11 @@ export class DishService {
   getDishes(): Observable<Dish[]> {
     return of(DISHES).pipe(delay(2000))
   }
+  getDishIds(): Observable<string[] | any> {
+    return of(DISHES.map(dish => dish.id ));
+  }
 
-  getDish(id: number): Observable<Dish> {
+  getDish(id: string): Observable<Dish> {
     return of(DISHES.filter((dish) => (dish.id === id))[0]).pipe(delay(2000))
   }
 
--- src/app/shared/dish.ts
+++ src/app/shared/dish.ts
@@ -1,6 +1,6 @@
 import {Comment} from './comment'
 export class Dish {
-    id: number;
+    id: string;
     name: string;
     image: string;
     category: string;
